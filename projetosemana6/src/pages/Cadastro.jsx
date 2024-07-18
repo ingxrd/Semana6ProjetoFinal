2611b105dc9e1c9db1c075f0587878c3dbb82d96
unknown <dobsfoxx@gmail.com>
2024-07-18 15:58:45 -0300

    Atualização Login e cadastro, ainda nao finalizado

--- projetosemana6/src/pages/Cadastro.jsx
+++ projetosemana6/src/pages/Cadastro.jsx
@@ -1,7 +1,80 @@
+import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { cadastrarUsuario, entrarGoogle } from "../firebase/auth";
+import toast from "react-hot-toast";
+
+
+
 function Cadastro() {
-    return(
-        <h1>Cadastro</h1>
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate()
+
+    function cadastrar(data) {
+        cadastrarUsuario(data.nome, data.email, data.senha).then(() => {
+            toast.success(`Olá ${data.nome}, você está logado!`);
+            navigate("/home");
+        }).catch((error) => {
+            toast.error("Um erro aconteceu:" + error.code)
+        });
+    }
+    function handleEntrarGoogle() {
+        entrarGoogle().then(() => {
+            toast.success("Bem vindo!");
+            navigate("/home");
+        });
+    }
+
+    return (
+        <main className="main" style={{ backgroundImage: `url("fundo.jpg")` }} >
+            <form className=" container mt-5 text-center form-area form-section" onSubmit={handleSubmit(cadastrar)}>
+                <h1>Cadastro</h1>
+                <hr />
+                <div>
+                    <label htmlFor="nome">Nome</label>
+                    <input
+                        type="text"
+                        id="nome"
+                        className="form-control"
+                        {...register("nome", { required: true, maxLength: 100 })}
+                    />
+                    {errors.nome && <strong>Nome inválido!</strong>}
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        className="form-control"
+                        {...register("email", { required: true })}
+                    />
+                    {errors.email && <strong>Email inválido</strong>}
+                </div>
+                <div>
+                    <label htmlFor="senha">Senha</label>
+                    <input
+                        type="password"
+                        id="senha"
+                        className="form-control"
+                        {...register("senha", { required: true, minLength: 6 })}
+                    />
+                    {errors.senha && <strong>Senha inválida!</strong>}
+                </div>
+                <div className="container d-flex flex-column btn-area" fluid>
+                    <Button variant="dark" className="mt-3 " type="submit">
+                        Cadastrar
+                    </Button>
+                    <Button
+                        loading
+                        onClick={handleEntrarGoogle}
+                        variant="warning"
+                        className="mt-1"
+                        type="button">
+                        Entrar com o Google
+                    </Button>
+                </div>
+            </form>
+        </main>
     )
 }
-
 export default Cadastro;
