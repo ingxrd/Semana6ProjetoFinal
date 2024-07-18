dc199e9caf6a64a84e8b77d97a77475b57f3ac2b
ingxrd <ingridmunhoz86@gmail.com>
2024-07-18 15:00:18 -0300

    atualizações - pagina livros

--- projetosemana6/src/pages/Livros.jsx
+++ projetosemana6/src/pages/Livros.jsx
@@ -1,6 +1,16 @@
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom"
+
 function Livros(){
     return(
-        <h1>Livros</h1>
+        <main>
+            <Container className="mt-5">
+            <h1>Meus Livros</h1>
+            <hr/>
+            <Link className="btn btn-dark" to="/livros/adicionar">Adicionar Novo Livro</Link>
+            </Container>
+
+        </main>
     )
 }
 
